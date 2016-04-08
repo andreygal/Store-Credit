fca06d40a841ad7aaafd2ac410121c52592c4d13
Andrey Galper <andreygalper@gmail.com>
2016-04-08 12:26:50 -0400

    gcj

--- StoreCredit/main.cpp
+++ StoreCredit/main.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
+#include <sstream>
 #include <utility>
 #include <vector>
 #include "insertion_sort.h"
 #include "insert_descending.h"
 #include "store_credit.h"
 using namespace std; 
-//main program will test the store credit function using the items vector and a user entered value for credit
-int main() {
+
+int main(int argv, char* argc) {
 	int num_cases, i{ 0 };
 	cin >> num_cases; 
 	while (i < num_cases) {
 		int k, credit; 
 		cin >> credit;
+		string line;
+		getline(cin, line);
+		istringstream iss; 
 		vector<int> items; 
-		while (cin >> k) {
+		while (iss >> k) {
 			items.emplace_back(k);
 		}
 		pair<int, int> result = StoreCredit(credit, items);
