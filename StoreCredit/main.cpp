cac291b883675fd6df430234ea462636e692bef8
Andrey Galper <andreygalper@gmail.com>
2016-04-09 00:14:20 -0400

    adjust to gcj spec

--- StoreCredit/main.cpp
+++ StoreCredit/main.cpp
@@ -7,14 +7,18 @@
 using namespace std; 
 //main program will test the store credit function using the items vector and a user entered value for credit
 int main() {
-	vector<int> items = { 6, 2, 8, 3, 9, 6, 7 };
-	int credit{ 0 };
-	cout << "Please enter your store credit: "; 
-	cin >> credit; 
-	pair<int, int> my_pair = StoreCredit(credit, items);
-	cout << "According to the items price list, your credit add up to items "; 
-	cout << (my_pair.first + 1) << " and " << (my_pair.second + 1) << endl;
-	cout << items[my_pair.first] << " + " << items[my_pair.second] << " = " << credit << endl; 
+	int num_cases, i{ 0 };
+	cin >> num_cases; 
+	while (i < num_cases) {
+		int k, credit; 
+		cin >> credit;
+		vector<int> items; 
+		while (cin >> k) {
+			items.emplace_back(k);
+		}
+		pair<int, int> result = StoreCredit(credit, items);
+		cout << "Case #" << i + 1 << ": " << result.first << " " << result.second << endl; 
+	}
 
 	system("pause");
 
